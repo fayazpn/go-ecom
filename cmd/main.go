6b48b845e781bf03cf0fbf6dfa9c9fc356932e88
Fayaz Basheer <pnfayaz@gmail.com>
2024-09-30 15:49:00 +0400

    set up http server and user service

---
+++ cmd/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/fayazpn/ecom/cmd/api"
+	"github.com/fayazpn/ecom/config"
+	"github.com/fayazpn/ecom/db"
+	"github.com/go-sql-driver/mysql"
+)
+
+func main() {
+
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	// connect the db
+	initStorage(db)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := api.NewAPIServer(":8081", nil)
+
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DB: Successfully connected")
+
+}
